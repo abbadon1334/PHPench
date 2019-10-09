e107cf99adef93e1385a34a3bf26f35dd6da0d4e
abbadon1334 <fdanti@gmail.com>
2019-10-09 19:33:07 +0300

    - added Console Ranking Table On Repetition (optional) - added Console Ranking Table Final (optional)

--- src/mre/PHPench.php
+++ src/mre/PHPench.php
@@ -2,10 +2,12 @@
 
 namespace mre;
 
+use LucidFrame\Console\ConsoleTable;
 use mre\PHPench\Aggregator\SimpleAggregator;
 use mre\PHPench\AggregatorInterface;
-use mre\PHPench\Output\OutputInterface;
 use mre\PHPench\BenchmarkInterface;
+use mre\PHPench\Output\OutputInterface;
+use mre\PHPench\Util\Math;
 use SebastianBergmann\Timer\Timer;
 
 /**
@@ -13,7 +15,7 @@ use SebastianBergmann\Timer\Timer;
  *
  * This class provides the core functionality for the PHPench package.
  *
- * @link http://github.com/mre/PHPench
+ * @link   http://github.com/mre/PHPench
  *
  * @author Matthias Endler <matthias-endler@gmx.net>
  * @author Markus Poerschke <markus@eluceo.de>
@@ -47,8 +49,16 @@ class PHPench
      * @var int
      */
     private $repetitions = 3;
+    /**
+     * @var bool
+     */
+    private $show_table_final = false;
+    /**
+     * @var bool
+     */
+    private $show_table_pass = false;
 
-    public function __construct(AggregatorInterface $aggregator = null)
+    public function __construct(?AggregatorInterface $aggregator = null)
     {
         if (null === $aggregator) {
             $aggregator = new SimpleAggregator();
@@ -98,9 +108,17 @@ class PHPench
 
                 $this->output->update($this->aggregator, $i);
             }
+
+            if ($this->show_table_pass) {
+                $this->showTableData($this->output->getTestsTitles(), $this->aggregator->getData(), $r, false);
+            }
         }
 
         $this->output->finalize($this->aggregator, $i);
+
+        if ($this->show_table_final) {
+            $this->showTableData($this->output->getTestsTitles(), $this->aggregator->getData(),0, true);
+        }
 
         if ($keepAlive) {
             // Wait for user input to close
@@ -139,5 +157,66 @@ class PHPench
         }
 
         $this->aggregator->push($i, $index, $time);
+    }
+
+    public function setShowRankingTable(bool $on_final = true, bool $every_pass = false): void
+    {
+        $this->show_table_final = true;
+        $this->show_table_pass = $every_pass;
+    }
+
+    private function showTableData($titles, $data, $passage, $final = true): void
+    {
+        if (false === $final) {
+            echo "\e[31mPartial Rank #" . $passage . "\e[0m" . PHP_EOL;
+        } else {
+            echo "\e[33mFinal Rank \e[0m" . PHP_EOL;
+        }
+
+        $table = new ConsoleTable();
+        $table->addHeader('Test');
+        $table->addHeader('Avg');
+        $table->addHeader('Min');
+        $table->addHeader('Max');
+        $table->addHeader('Std');
+        $table->addHeader('Range');
+
+
+        $ranked = [];
+        foreach ($titles as $titleIndex => $title) {
+
+            $ranked[$title] = [];
+            foreach($data as $rowIndex => $rowData) {
+                $ranked[$title][$rowIndex] = $rowData[$titleIndex];
+            }
+
+            $avg = array_sum($ranked[$title]) / count($ranked[$title]);
+
+            $min = min($ranked[$title]);
+            $max = max($ranked[$title]);
+
+            $std = Math::standardDeviation($ranked[$title]);
+            $range = $max - $min;
+            $ranked[$title] = [
+                $title,
+                number_format($avg,10),
+                number_format($min,10),
+                number_format($max,10),
+                number_format($std,10),
+                number_format($range,10)
+            ];
+        }
+
+        uasort($ranked, function($a,$b) {
+            if ($a[1] === $b[1]) return 0;
+
+            return ($a[1] < $b[1]) ? -1 : 1;
+        });
+
+        foreach($ranked as $row) {
+            $table->addRow($row);
+        }
+
+        $table->display();
     }
 }
